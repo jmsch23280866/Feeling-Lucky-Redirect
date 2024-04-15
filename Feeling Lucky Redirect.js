dc74f36715338710f79238ff0e44cea396d48a70
Agent_E04 <58344071+jmsch23280866@users.noreply.github.com>
2024-04-15 14:59:49 +0800

    Update Feeling Lucky Redirect.js

--- Feeling Lucky Redirect.js
+++ Feeling Lucky Redirect.js
@@ -1,24 +1,20 @@
 // ==UserScript==
-// @name         Google I'm Feeling Lucky Auto Redirect
+// @name         Google 跳轉直接連結
 // @namespace    https://github.com/jmsch23280866/
-// @version      0.1
-// @description  在 Google 好手氣跳轉頁面自動點擊網址
+// @version      0.2
+// @description  Google 好手氣直接跳轉網頁
 // @author       Agent E04
-// @match        https://www.google.com/url?q=*
+// @match        *://www.google.com/url?q=*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // 在頁面加載後，自動點擊指定的元素
-    window.addEventListener('load', function() {
-        // 在這裡選擇要點擊的元素，這是一個示例選擇器
-        var elementToClick = document.querySelector('body > div.fTk7vd > a:nth-child(1)');
-        
-        // 如果找到了元素，則進行點擊
-        if (elementToClick) {
-            elementToClick.click();
-        }
-    });
+    // 檢查當前網址是否包含指定的前段
+    if (window.location.href.startsWith("https://www.google.com/url?q=")) {
+        // 將指定前段刪去並跳轉至剩餘的部分
+        var url = window.location.href.replace("https://www.google.com/url?q=", "");
+        window.location.href = decodeURIComponent(url); // 解碼網址中的特殊字符
+    }
 })();
